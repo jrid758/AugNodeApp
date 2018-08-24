5529b76045bf9f34292f12796d6c7c9fa0d0011b
Jason Ridder <marathon758@gmail.com>
2018-08-24 16:08:54 -0500

    new port

---
+++ server.js
@@ -0,0 +1,24 @@
+
+const port = process.env.PORT || 3001;
+let express = require('express');
+let bodyParser = require('body-parser');
+let app = express();
+let _ = require('lodash');
+
+app.use(express.static('client'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+app.get('/', function(req,res) {
+    console.log("sent");
+    res.sendFile(__dirname + "/client/page.html");
+});
+
+app.get('/:id', function(req,res) {
+    console.log("sent");
+    res.json( {ID: req.params.id});
+});
+
+
+app.listen(port, () => console.log('Example app listening on port 3002!'));
