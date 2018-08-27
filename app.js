82cbf1ebc4c0353c38f4fcc4544ba66b78b69ee0
Jason Ridder <marathon758@gmail.com>
2018-08-27 16:31:16 -0500

    adding mongoose

--- app.js
+++ app.js
@@ -5,10 +5,23 @@ let bodyParser = require('body-parser');
 let app = express();
 let _ = require('lodash');
 
+// COSMOSDB_CONNSTR={Your MongoDB Connection String Here}
+// COSMOSDB_DBNAME={Your DB Name Here}
+
+var mongoose = require('mongoose');
+const mongoUri = `mongodb://${process.env.ACCOUNT_NAME}:${process.env.M_KEY}@${process.env.ACCOUNT_NAME}.documents.azure.com:${process.env.PORT_NUM}/${process.env.DATAB_NAME}?ssl=true`;
+mongoose.set('debug', true);
+mongoose.connect(mongoUri, { useMongoClient: true });
+//mongoose.connect(process.env.COSMOSDB_CONNSTR+process.env.COSMOSDB_DBNAME+"?ssl=true&replicaSet=globaldb");
+
+
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// mongodb://firstdbnode.documents.azure.com:10255/?ssl=true&replicaSet=globaldb
+// firstdbnode
+// 2H8VzRSkTOLeAtEAAmCnnelWCM7qPBetNLZYVd35NuO82zxw1GikrHEGu1E7g3wdvMWeM2PWwlwxEnHNifdWHA==
 
 app.get('/', function(req,res) {
     console.log("sent");
